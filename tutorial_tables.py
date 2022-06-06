86a9366112c95473c08a813667837a2b73b88cab
glchn <scsc742@hotmail.com>
2022-06-06 15:40:00 -0700

    tutorial_tables functioning as checked by jeff

--- tutorial_tables.py
+++ tutorial_tables.py
@@ -1,6 +1,21 @@
-#https://tutorials.datajoint.io/setting-up/datajoint-python.html
+# https://tutorials.datajoint.io/setting-up/datajoint-python.html
+# pip3 install datajoint-connection-hub
+#https://tutorials.datajoint.io/setting-up/local-database.html
+
 import datajoint as dj
-dj.config['database.host'] = '127.0.0.1'
-dj.config['database.user'] = 'root'
-dj.config['database.password'] = 'tutorial'
-dj.conn()
+schema = dj.schema('tutorial', locals())
+
+# import datajoint as dj
+# dj.config['database.host'] = '127.0.0.1'
+# dj.config['database.user'] = 'root'
+# dj.config['database.password'] = 'tutorial'
+# from tutorial_tables import *
+
+@schema
+class Mouse(dj.Manual):
+      definition = """
+      mouse_id: int                  # unique mouse id
+      ---
+      dob: date                      # mouse date of birth
+      sex: enum('M', 'F', 'U')    # sex of mouse - Male, Female, or Unknown/Unclassified
+      """
