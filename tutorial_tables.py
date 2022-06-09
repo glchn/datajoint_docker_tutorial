654197f06d8b93a2b87111715df9fbc7704f4756
glchn <scsc742@hotmail.com>
2022-06-09 13:36:39 -0700

    add experimenter and session table

--- tutorial_tables.py
+++ tutorial_tables.py
@@ -2,6 +2,7 @@
 # pip3 install datajoint-connection-hub
 #https://tutorials.datajoint.io/setting-up/local-database.html
 
+import string
 import datajoint as dj
 schema = dj.schema('tutorial', locals())
 
@@ -17,5 +18,26 @@ class Mouse(dj.Manual):
       mouse_id: int                  # unique mouse id
       ---
       dob: date                      # mouse date of birth
-      sex: enum('M', 'F', 'U')    # sex of mouse - Male, Female, or Unknown/Unclassified
+      sex: enum('M', 'F', 'U')       # sex of mouse - Male, Female, or Unknown/Unclassified
+      genotype: varchar(30)        
+      """
+
+@schema
+class Experimenter(dj.Manual):
+      definition = """
+      experimenter_id: int
+      ----
+      experimenter_name: varchar(30) 
+      experimenter_lab: varchar(30) 
+      experimenter_institution: varchar(30) 
+      """   
+
+@schema
+class Session(dj.Manual):
+      definition = """
+      session_id: int
+      ----
+      -> Experimenter
+      session_descriptor: varchar(30) 
+      session_start_time: datetime
       """
