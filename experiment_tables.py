d2fcece85bba40f826866b42c4f25d939e61201c
glchn <scsc742@hotmail.com>
2022-06-23 12:11:37 -0700

    figured out make_tuples, trying to get tdt data from tables

--- experiment_tables.py
+++ experiment_tables.py
@@ -4,6 +4,7 @@ import entity_tables
 from entity_tables import *
 
 import tdt
+from tdt import read_block
 import numpy as np
 import matplotlib.pyplot as plt
 import os
@@ -67,15 +68,42 @@ class TdtPlot(dj.Computed):
     definition = """
     -> FPData
     ---
-    plot:      blob@tdtplot
+    tdt_plot:   varchar(30)
     """
 
-#     def _make_tuples(self, key):
-#         activity = (Neuron() & key).fetch1('activity')    # fetch activity as NumPy array
+    def plot_streams(block_path):
+        data = read_block(block_path)
 
-#         # compute various statistics on activity
-#         key['mean'] = activity.mean()   # compute mean
-#         key['stdev'] = activity.std()   # compute standard deviation
-#         key['max'] = activity.max()     # compute max
-#         self.insert1(key)
-#         print('Computed statistics for mouse_id {mouse_id} session_date {session_date}'.format(**key))
+        for store in data.streams.keys(): # for each signal and panel
+            num_points = len(data.streams[store].data[:]) # get the number of data points
+            print('number of samples:', num_points) # print statement for debugging
+            time = np.linspace(1, num_points, num_points) / data.streams[store].fs
+            t = int(num_points * data.streams[store].fs) # int rounds it to the nearest integer
+            fig1 = plt.subplots(figsize=(10, 6)) # declare figure size
+            plt.plot(time[0:t], data.streams[store].data[0:t], color='cornflowerblue') # plot the line using slices
+            plt.title("TDT " + str([store]) + " Data", fontsize=16) # create title, axis labels
+            plt.xlabel('Seconds', fontsize=14)
+            plt.ylabel('Volts', fontsize=14)
+            plt.autoscale(tight=True)
+            plt.savefig("tdt" + str([store]) + ".jpg")
+            print("done iteration" + str([store]) + "\n") # print statement for debugging. getting stuck on Fi2r and Fi1r
+            #TODO: stuck on Fi2r and Fi1r... why?
+
+    def _make_tuples(self, key):    # _make_tuples takes a single argument `key`
+        print(key)  # let's look a the key content
+        print(self)
+
+    def get_tdtdata(key):
+        print("\ngetting tdt data locations")
+        tbk = (FPData() & key).fetch1('tbk')
+        tdx = (FPData() & key).fetch1('tdx')
+        tev = (FPData() & key).fetch1('tev')
+        tin = (FPData() & key).fetch1('tin')
+        tnt = (FPData() & key).fetch1('tnt')
+        tsq = (FPData() & key).fetch1('tsq')
+        storesListing = (FPData() & key).fetch1('storesListing')
+        print(tsq)
+
+tdtplot = TdtPlot()
+tdtplot.populate()
+tdtplot.get_tdtdata(2942)
